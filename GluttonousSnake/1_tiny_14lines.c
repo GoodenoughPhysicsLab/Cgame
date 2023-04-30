a59147cae58c01b67357df3aac5845a9d439080b
Goodenough <2381642961@qq.com>
2023-04-30 13:15:58 +0800

    初步整理了代码格式

--- GluttonousSnake/1_tiny_14lines.c
+++ GluttonousSnake/1_tiny_14lines.c
@@ -1,22 +1,43 @@
 #include <stdlib.h>
 #include <conio.h>
-int main() {
-    int W = 20, S = W * W, *m, z[2] = {0}, l = 3, i, c = 'D', C, *p, f;
-    for (srand(m = calloc(S, 4)), C = m[1] = -1; C - 27; _sleep(100)) {
-        if (_kbhit())C = _getch() & 95, C - 65 && C - 68 &&
-            C - 83 && C - 87 || (C ^ c) & 20 ^ 4 && (c = C);
-        p = z + !!(c & 2), *p += c / 3 & 2, *p = (--*p + W) % W;
-        f = !system("cls"), *(p = m + *z + z[1] * W) > 0 && (C = 27);
-        for (; *p && (m[i = rand() % S] || (--m[i], ++l, --f)););
-        for (i = 0, *p = l; i < S; ++i % W || _cputs("|\n"))
-            _cputs(m[i] > 0 ? m[i] -= f, "()" : m[i] ? "00" : "  ");
+
+int main() 
+{
+    int mapWide = 20,
+        size = mapWide * mapWide,
+        *map,
+        position[2] = {0}, 
+        length = 3, i, 
+        behave = 'D', // 'A', 'W', 'S', 'D'
+        C, // 更新动作前的缓冲，获取输入，A, W, S, D中的一种 蛇的方向
+        *p, // 临时变量
+        food;
+
+    for (srand(map = calloc(size, 4)), C = map[1] = -1; C - 27; _sleep(100))
+    {
+        if (_kbhit()) {
+            C = _getch() & 95, 
+            C - 65 && C - 68 && C - 83 && C - 87 || (C ^ behave) & 20 ^ 4 && (behave = C);
+       }
+        p = position + !!(behave & 2),
+        *p += behave / 3 & 2, 
+        *p = (--*p + mapWide) % mapWide;
+
+        food = !system("cls"), 
+        *(p = map + *position + position[1] * mapWide) > 0 && (C = 27);
+
+        for (; *p && (map[i = rand() % size] || (--map[i], ++length, --food)););
+
+        for (i = 0, *p = length; i < size; ++i % mapWide || _cputs("|\n")) {
+            _cputs(map[i] > 0 ? map[i] -= food, "()" : map[i] ? "00" : "  ");
+        }
     }
 }
 
 /*
 14行贪吃蛇, 668字符, 实现了简单的功能;
 ADSW移动, 吃到食物成长, 可穿墙, 吃到自己身体或按下Esc键时游戏结束;
-可自定义蛇头位置(z[0], z[1]), 蛇长(l), 初始方向(c), 地图尺寸(W);
+可自定义蛇头位置(position[0], position[1]), 蛇长(length), 初始方向(behave), 地图尺寸(mapWide);
 在15行的版本上加以改进, 增加了Esc键退出, 更方便的自定义地图大小;
 注意: 初次食物生成是固定位置, 默认情况下会在第一轮循环后重新生成
 (除非你手速特别快, 或者修改了默认参数, 一般都不用在意这点细节)
